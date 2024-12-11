3e8c00eda81f7015f03589361f792de322a8d152
林亮均 <144506404+LinLiangChun@users.noreply.github.com>
2024-12-11 13:58:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -393,7 +393,7 @@ if __name__ == "__main__":
 
     if args.bench_name.startswith("classification"):
         #max_tokens = 16
-        max_tokens = 256
+        max_tokens = 128
         agent_name = ClassificationAgent
     elif args.bench_name.startswith("sql_generation"):
         max_tokens = 512
